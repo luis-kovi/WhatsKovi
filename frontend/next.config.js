30cdc16df18fb2acc67c47db38e4957acad36f94
Luis Fernando Alves <luis.alves@kovi.com.br>
2025-11-05 19:32:51 -0300

    ajustes de login

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
+  },
 }
 
 module.exports = nextConfig
